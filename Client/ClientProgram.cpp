7c6c717c061b4882765454d9ba2b12d7d5ff8963
Anna-Sobchuk <77459095+Anna-Sobchuk@users.noreply.github.com>
2023-11-28 05:00:54 +0200

    Message read by ClientProgram.cpp
    Successfully found words in a message the user wants to find.

--- Client/ClientProgram.cpp
+++ Client/ClientProgram.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
 #include <asio.hpp>
+#include <regex>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+std::vector<std::string> GetWords() {
+    std::cout << "Please enter the words you want to find: ";
+    std::string input;
+    std::getline(std::cin, input);
+
+    std::regex word_regex(R"([^(0-9|a-z|A-Z|')]+)");
+    std::sregex_token_iterator iter(input.begin(), input.end(), word_regex, -1);
+    std::sregex_token_iterator end;
+
+    std::vector<std::string> words;
+    for (; iter != end; ++iter) {
+        std::string word = iter->str();
+        if (!word.empty()) {
+            std::transform(word.begin(), word.end(), word.begin(), ::tolower);
+            words.push_back(word);
+        }
+    }
+
+    return words;
+}
 
 int main() {
     try {
@@ -9,17 +34,12 @@ int main() {
         socket.connect(asio::ip::tcp::endpoint(asio::ip::address::from_string("127.0.0.1"), 5001));
         std::cout << "Connected to server!" << std::endl;
 
-        // Send a message to the server
-        std::string message = "Hello from client!\n";
-        asio::write(socket, asio::buffer(message));
+        //Read the words
+        std::vector<std::string> words = GetWords();
+        for (const auto& word : words) {
+            std::cout << " " << word;
+        }
 
-        // Receive and print the response from the server
-        asio::streambuf receive_buffer;
-        asio::read_until(socket, receive_buffer, '\n');
-        std::istream input_stream(&receive_buffer);
-        std::string response;
-        std::getline(input_stream, response);
-        std::cout << "Received response from server: " << response << std::endl;
     }
     catch (const std::exception& e) {
         std::cerr << "Error: " << e.what() << std::endl;
